d2920528d4a7d1cc201049376288237dd7ad5778
Danny-16gg" <ccorte22@estudiante.iberoamericana.edu.co>
2025-03-20 19:41:09 -0500

    actualizacion 0.0.1

--- src/proyectodanielycarlos/ProyectoDanielyCarlos.java
+++ src/proyectodanielycarlos/ProyectoDanielyCarlos.java
@@ -1,20 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+/*vamos a crear un un projecto de cajero  */
 package proyectodanielycarlos;
 
-/**
- *
- * @author Daniel sexy
- */
+import javax.swing.JOptionPane;
+
+
 public class ProyectoDanielyCarlos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        //vamos a realizar el ingreso a la app
+        //Ingreso:Clave
+        final int usuario=2212;
+        int i=1,j=1,ventaT=0;
+        String [] opciones={"Registrar venta","Ventas del día", "Salir"};
+        
+        while(i==1){
+            int clave = Integer.parseInt(JOptionPane.showInputDialog("Ingrese contraseña: "));
+            
+            //panel de opciones:
+                
+            if (clave==usuario){
+                
+                while (i==1){
+                    int opcion=JOptionPane.showOptionDialog(null, "Escoge una opción: ", "Menu de opciones: ", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+                    
+                    switch (opcion){
+                        case 0:
+                        {
+                            while (i==1){
+                                int venta = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor: "));
+                                    ventaT=ventaT+venta;
+                                    if (venta == 0){
+                                        break;
+                                    }                                  
+                                }                 
+                                JOptionPane.showMessageDialog(null, "El total de la venta es: "+ ventaT);                            
+                                break;
+                        }
+                        case 1:
+                        {
+                            JOptionPane.showMessageDialog(null, "La venta fue de: "+ ventaT);
+                            break;
+                        }
+                        case 2:
+                            i=0;                      
+                        
+                    }                    
+                }
+            }
+            //Reinicio del código:
+            int resp=JOptionPane.showConfirmDialog(null,"¿Desea continuar?");
+            
+            if (resp==JOptionPane.YES_OPTION){
+                i=1;
+            }
+            else {
+                break;
+            }
+        }     
+    }    
 }
